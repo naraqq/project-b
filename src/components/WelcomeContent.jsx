4ce9924d5b29c4056914eabbb6ebc7c5b56ff164
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-07-03 15:19:29 +0800

    update

--- src/components/WelcomeContent.jsx
+++ src/components/WelcomeContent.jsx
@@ -1,41 +1,42 @@
+import { useEffect, useState } from "react";
 import OurPartners from "./OurPartners";
-
+import axios from "axios";
+import { useMediaQuery } from "react-responsive";
+import { useNavigate } from "react-router-dom";
 function WelcomeContent() {
-  const collection = [
-    [
-      {
-        img: "done/1.jpg",
-      },
-      {
-        img: "done/2.jpg",
-      },
-      {
-        img: "done/3.jpg",
-      },
-    ],
-    [
-      {
-        img: "done/4.jpg",
-      },
-      {
-        img: "done/5.jpg",
-      },
-      {
-        img: "done/6.jpg",
-      },
-    ],
-    [
-      {
-        img: "done/7.jpg",
-      },
-      {
-        img: "done/8.jpg",
-      },
-      {
-        img: "done/9.jpg",
-      },
-    ],
-  ];
+  const navigate = useNavigate();
+  const isMobile = useMediaQuery({ maxWidth: 555 });
+  const isTablet = useMediaQuery({ minWidth: 556, maxWidth: 1023 });
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  let gridColumns = "grid-cols-1";
+  if (isTablet) {
+    gridColumns = "grid-cols-2";
+  } else if (isDesktop) {
+    gridColumns = "grid-cols-3";
+  }
+  const [loading, setLoading] = useState(false);
+  const [news, setNews] = useState();
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/v1/News/getNews`, {
+        headers: {
+          "Content-Type": "applicaiton/json",
+        },
+      })
+      .then((res) => {
+        let tempo = [];
+        for (let i = 0; i < res.data.news.length; i++) {
+          const element = res.data.news[i];
+          if (element.newsType == "2") {
+            tempo.push(element);
+          }
+        }
+        setNews(tempo);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <div className="body-cus wrapper">
@@ -162,23 +163,64 @@ function WelcomeContent() {
                 <h1 className=" nunito-600 uppercase mb-5">
                   Бүтээн байгуулалт
                 </h1>
-                {collection.map((container, index) => {
-                  return (
-                    <div key={index} className="row">
-                      {container.map((item, ind) => {
+                {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+                  {news?.map((item, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`cursor-pointer hover:shadow p-3 rounded`}
+                      >
+                        <img
+                          className={`  center-block h-[320px] w-full rounded`}
+                          src={`${item.imgPath}`}
+                          alt="By Håkon Sataøen"
+                        />
+                        <p className="mt-2 text-[16px] !text-justify nunito-400 truncate">
+                          {item.title}
+                        </p>
+                        <p className="mt-2 text-[16px] !text-justify nunito-400 truncate">
+                          {item.newsBody}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div> */}
+                <div className="">
+                  <div className="">
+                    <div className={`grid ${gridColumns} gap-4`}>
+                      {news?.map((info, index) => {
                         return (
-                          <div key={ind} className="col-md-4">
+                          <div
+                            key={index}
+                            onClick={() => {
+                              navigate("/news-detail", {
+                                state: info,
+                              });
+                            }}
+                            className={` p-2 rounded h-[400px] hover:scale-105 transition-all font-bold overflow-hidden !text-[#323232] cursor-pointer`}
+                          >
                             <img
-                              className="center-block h-[320px] w-full shadow"
-                              src={`${item.img}`}
-                              alt="By Håkon Sataøen"
+                              src={info.imgPath}
+                              alt=""
+                              className="rounded-cus shadow-sm h-[300px] w-full"
                             />
+                            <div className="h-[50px] w-full">
+                              <p className="nunito-500 text-sm mt-2 truncate !font-[900]">
+                                {info.title}
+                              </p>
+                              <div className="nunito-500 !font-[300] text-sm mt-2 truncate text-gray-500">
+                                {info.description}
+                              </div>
+                            </div>
+                            <div className="nunito-500 !font-[300] text-sm text-gray-500 w-full flex justify-start">
+                              {info.createdDate}
+                            </div>
                           </div>
                         );
                       })}
                     </div>
-                  );
-                })}
+                  </div>
+                </div>
               </div>
             </section>
           </div>
